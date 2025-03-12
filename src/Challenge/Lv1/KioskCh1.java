0c0ca4ea5312cf2e309768ec1cbafb139d5a4a9f
minjee2758 <minjee2758@naver.com>
2025-03-12 18:17:59 +0900

    KioskCh1클래스에 장바구니 기능 추가
    OrderList클래스에서 모든 주문 내역, 가격 관리함.
    
    KioskCh1클래스에 OrderList 불러와서 장바구니 기능 구현

--- src/Challenge/Lv1/KioskCh1.java
+++ src/Challenge/Lv1/KioskCh1.java
@@ -8,6 +8,7 @@ public class KioskCh1 {
     Scanner scanner = new Scanner(System.in); //스캐너 불러오기
     MenuCh1 menu = new MenuCh1();
     List<String> category = new ArrayList<>(menu.getMenuCategory().keySet());
+    OrderList orderList = new OrderList();
 
     //키오스크 실행하기
     public void start(){
@@ -24,20 +25,56 @@ public class KioskCh1 {
                 System.out.println(i++ +". "+s);
             }
             System.out.println("0. 키오스크 종료");
+
+            //장바구니 리스트 보여주기
+            if (orderList.getOrderList().size()>0) {
+                System.out.println("[ Order Menu ]");
+                System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.\n"
+                    + "5. Cancel       | 진행중인 주문을 취소합니다.");
+            }
+
             //메뉴 입력 받기
-            System.out.print("→ 원하는 카테고리의 번호를 입력하세요 : ");
+            System.out.print("→ 원하는 번호를 입력하세요 : ");
             int select = scanner.nextInt();
             try {
-                if (select == 0){
-                    System.out.println("::::::::::::::::::::::::::::::::::::::::::::::");
-                    System.out.println("프로그램을 종료합니다.");
-                    break;
+                if (orderList.getOrderList().size()==0){
+                    if (select == 0){
+                        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::");
+                        System.out.println("프로그램을 종료합니다.");
+                        break;
+                    } else {
+                        String menuCategory = findMenuCategory(select);
+                        System.out.println("> "+ menuCategory+"를 선택하셨습니다 <");
+                        System.out.println();
+                        System.out.println("[ "+menuCategory+" ]");
+                        SelectMenuDetail(menuCategory); //세부 메뉴로 넘어가기
+                    }
                 } else {
-                    String menuCategory = findMenuCategory(select);
-                    System.out.println("> "+ menuCategory+"를 선택하셨습니다 <");
-                    System.out.println();
-                    System.out.println("[ "+menuCategory+" ]");
-                    SelectMenuDetail(menuCategory); //세부 메뉴로 넘어가기
+                    if (select == 0){
+                        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::");
+                        System.out.println("프로그램을 종료합니다.");
+                        break;
+                    } else if (select == 4) {
+                        orderList.printOrderList();
+                        System.out.println("\n" +"[ Total ] \n" + "W " + orderList.getTotalPrice());
+                        System.out.println("1. 주문      2. 메뉴판");
+                        int lastSelect = scanner.nextInt();
+                        if (lastSelect == 1){
+                            System.out.println("주문이 완료되었습니다. 금액은 W "+orderList.getTotalPrice()+" 입니다.");
+                            break;
+                        } else {
+                            System.out.println("다시 메뉴판으로 이동합니다");
+                        }
+                    } else if (select==5) {
+                        System.out.println("주문 취소합니다");
+                        break;
+                    } else {
+                        String menuCategory = findMenuCategory(select);
+                        System.out.println("> "+ menuCategory+"를 선택하셨습니다 <");
+                        System.out.println();
+                        System.out.println("[ "+menuCategory+" ]");
+                        SelectMenuDetail(menuCategory); //세부 메뉴로 넘어가기
+                    }
                 }
             } catch (IllegalArgumentException e){
                 System.out.println("error : "+e.getMessage()); //에러 뱉기
@@ -65,19 +102,14 @@ public class KioskCh1 {
                 } else {
                     String MenuName = findMenu(DetailMenu, select);
                     System.out.println("> "+MenuName+"를 선택하셨습니다 <");
-                    System.out.println();
+                    //장바구니에 추가하기
+                    orderList.AddOrderList(DetailMenu.get(select-1).getName(), DetailMenu.get(select-1).getPrice(),DetailMenu.get(select-1).getDescription());
                 }
             } catch (IllegalArgumentException e){
                 System.out.println("error : "+e.getMessage()); //에러 뱉기
             }
 
     }
-
-//    //가격 더하기
-//    public Double addPrice(int price){
-//        totalPrice += price;
-//        return totalPrice;
-//    }
 
     //카테고리명 찾기
     public String findMenuCategory(int select){
--- src/Challenge/Lv1/OrderList.java
+++ src/Challenge/Lv1/OrderList.java
@@ -2,12 +2,10 @@ package Challenge.Lv1;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class OrderList {
     private final List<MenuItemCh1> orderList = new ArrayList<>(); //장바구니 리스트
     private double totalPrice = 0; //총액 변수
-    Scanner scanner = new Scanner(System.in);
 
 
 
@@ -17,23 +15,19 @@ public class OrderList {
         totalPrice += price;
     }
 
-    //주문 내역 확인시켜주기
-    public void getOrderList() {
-        //장바구니에 물건이 들어있으면
-        if (orderList.size()>0){
-            System.out.println("[ Order Menu ]");
-            System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.\n"
-                    + "5. Cancel       | 진행중인 주문을 취소합니다.");
-            System.out.println("입력 | ");
-            int orderSelect = scanner.nextInt();
-            if (orderSelect==4){
-                System.out.println("아래와 같이 주문하시겠습니까? \n" +"[ Orders ]");
+    public List<MenuItemCh1> getOrderList(){
+        return orderList;
+    }
+
+    public void printOrderList(){
+        System.out.println("아래와 같이 주문하시겠습니까? \n" +"[ Orders ]");
                 for (MenuItemCh1 ordering : orderList) { //장바구니에 있는 메뉴 리스트 출력
                     System.out.println(ordering.getName() + "  | W "+ordering.getPrice() + "  | " + ordering.getDescription());
                 }
-                System.out.println("\n" +"[ Total ] \n" + "W " + totalPrice);
-            }
-        }
+    }
+
+    public double getTotalPrice(){
+        return totalPrice;
     }
 
     //주문을 종료하면 리스트 초기화하기
